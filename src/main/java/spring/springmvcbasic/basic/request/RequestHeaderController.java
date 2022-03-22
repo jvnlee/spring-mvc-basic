a411e38da3ea8dd26cddf8dc1d3c33c004b8482e
jvnlee <lhjun1028@naver.com>
2022-03-22 22:18:20 +0900

    [ADD] HTTP 요청 헤더 조회

---
+++ src/main/java/spring/springmvcbasic/basic/request/RequestHeaderController.java
@@ -0,0 +1,38 @@
+package spring.springmvcbasic.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+@Slf4j
+@RestController
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader("host") String host,
+                          @CookieValue(value = "myCookie", required = false, defaultValue = "yummy") String cookie
+                          ) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap); // 모든 header 항목
+        log.info("header host={}", host); // 특정 header 항목 (예시로 필수 항목인 host를 받아옴)
+        log.info("myCookie={}", cookie);
+
+        return "OK";
+
+    }
+
+}
